f9f729f8b9eeb112653395573b661fb21dd3752f
Artiol Thaqi <artiolthaqi06@gmail.com>
2025-05-03 14:42:26 +0200

    Lesson 3

--- wp-theme/index.php
+++ wp-theme/index.php
@@ -1,4 +1,11 @@
-
 <?php get_header(); ?>
-<h1>Digital School</h1>
+
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <small>Posted on: <?php the_time('F j,Y') ?>at <?php the_time('g:i a');?>in <?php the_category();?></small>
+        <p><?php the_content(); ?></p>
+    <?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
