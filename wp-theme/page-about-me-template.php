fd570203a29d63a23345e4fb3cbaa84c00ca139f
Blend Osamni <blendosmani010@gmail.com>
2025-05-10 14:36:21 +0200

    lesson 3

---
+++ wp-theme/page-about-me-template.php
@@ -0,0 +1,16 @@
+<?php 
+/* Template Name: About-me Template*/
+get_header(); ?>
+<?php
+if(have_posts()):
+    while(have_posts()): the_post(); ?>
+        <h1><?php the_title();?></h1>
+        <small> Posted on: <?php the_time('F j,Y')?> at <?php the_time('g:i a')?> in <?php the_category();?></small>
+        
+        <p><?php the_content();?></p>
+    <?php endwhile; ?>
+    <?php endif; ?>
+
+
+<?php  get_footer(); ?>
+
