a2bf6da501e438fbd9b45eee62667059a3af8f2f
AjanSs <ajansyla2@gmail.com>
2025-05-17 14:42:54 +0200

    lesson5

--- wp-theme/index.php
+++ wp-theme/index.php
@@ -7,7 +7,8 @@
    while(have_posts()): the_post(); ?>
    <h1><?php the_title(); ?></h1>
     <small>posted on: <?php the_time('F j,Y') ?> at <?php the_time('g:i a')?> in <?php the_category(); ?></small>
-   <p><?php the_content(); ?></p>
+   <div><?php the_post_thumbnail(array(200,200));?></div>
+    <p><?php the_content(); ?></p>
    <?php endwhile; ?>
    <?php endif; ?>
 <?php  get_footer(); ?>
