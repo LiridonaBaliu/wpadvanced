0afd2377e4ef0243bacb4285806ea24cfaac8e00
blendosmani <blendosmani010@gmail.com>
2025-05-17 14:44:38 +0200

    lesson 5

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -2,14 +2,42 @@
 function load_scripts(){
     wp_enqueue_style('style',get_template_directory_uri().'/style.css', false,'1.1','all');
     wp_enqueue_style('script',get_template_directory_uri().'/js/script.js',array(),'1.1',true);
+    wp_enqueue_style('bootstrap_style',get_template_directory_uri().'/css/bootstra.min.css',array(),'1.1',true);
+    wp_enqueue_style('bootstrap_js',get_template_directory_uri().'/js/bootstrap.bundle.js',array(),'1.1',true);
 
 }
 add_action('wp_enqueue_scripts','load_scripts');
 
-function ds_setup() {
-    add_theme_support('menus');
-    register_nav_menu('primary','primary Navigation');
+
+function config() {
+
+    register_nav_menus(
+
+        array(
+            'wp_devs_main_menu'=> 'Main Menu',
+            'wp_devs_footer_menu'=> 'Footer Menu',
+        )
+        );
+
+        $args = array(
+            'height' => 225,
+            'width' => 1920,
+        );
+        add_theme_support('custom-header',$args);
+        add_theme_support('post-thumbnails');
+        add_theme_support('cusotm-logo', array(
+            'height' =>100,
+            'width' => 200,
+            'flex-height' => true,
+            'flex-width' => true,
+        ));
+
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+        add_theme_support('title-tag');
 }
 
-add_action('init','ds_setup')
+
+
+add_action ('after_setup_theme', 'config',0);
 ?>
