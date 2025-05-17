3ab4a54eaafb7bc31a8444040677b4c4d22d3a70
Artiol Thaqi <artiolthaqi06@gmail.com>
2025-05-17 14:45:53 +0200

    lesson 5

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -1,15 +1,51 @@
 <?php
-function load_scripts() {
-    wp_enqueue_style('style', get_template_directory_uri()."/style.css", false, '1.1', 'all');
-    wp_enqueue_script('script',get_template_directory_uri()."/js/script.js",array(),'1.1',true);
+function load_scripts(){
+    wp_enqueue_style('wp_style',get_template_directory_uri()."/style.css",false,'1.1','all');
+    wp_enqueue_style('bootstrap_style',get_template_directory_uri()."wp-theme/css/bootstrap.min.css.map",false,'1.1','all');
+
+    wp_enqueue_script('wp_style',get_template_directory_uri()."/js/script.js",false,'1.1','true');
+    wp_enqueue_script('bootstrap_js',get_template_directory_uri()."wp-theme/js/bootstrap.bundle.min.js.map",false,'1.1','true');
+    
 }
-add_action('wp_enqueue_scripts', 'load_scripts');
+add_action('wp_enqueue_scripts','load_scripts');
 
-function ds_setup() {
-    add_theme_support('menus');
-    register_nav_menu('primary','Primary Navigation');
+
+function config() {
+
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+             'wp_devs_footer_menu' => 'Footer Menu',
+        )
+        );
+
+        $args = array(
+            'height' => 225,
+            'width' => 1920,
+        );
+        add_theme_support('custom-header',$args);
+        add_theme_support('post-thumbnails');
+        add_theme_support('custom-logo',array(
+            'height' => 110,
+            'width' => 200,
+            'flex-height' => true,
+            'flex-width' => true,
+        ));
+
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+        add_theme_support('title-tag');
+        
 
 }
 
-add_action('init','ds_setup')
+
+
+
+
+
+
+
+
+add_action('after_setup_theme','config',0);
 ?>
--- wp-theme/index.php
+++ wp-theme/index.php
@@ -4,6 +4,7 @@
     <?php while (have_posts()) : the_post(); ?>
         <h1><?php the_title(); ?></h1>
         <small>Posted on: <?php the_time('F j,Y') ?>at <?php the_time('g:i a');?>in <?php the_category();?></small>
+        <div><?php the_post_thumbnail(array(200,200));?></div>
         <p><?php the_content(); ?></p>
     <?php endwhile; ?>
 <?php endif; ?>
