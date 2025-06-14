772146c533c821ddcd3487875544e2ddde131220
Liridona Baliu <liridona.baliu@digitalschool.tech>
2025-06-14 14:31:42 +0200

    lesson 9

---
+++ wp-theme/page.php
@@ -0,0 +1,16 @@
+<?php  get_header(); ?>
+
+<img src="<?php header_image(); ?>"  height="<?php  echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width; ?>" alt="Header photo">
+
+<div id="id" class="site-content">
+   <div id="primary" class="content-area">
+     <main id="main" class="site-main">
+         <div class="container">
+         <div class="page-item">
+
+
+         </div>
+         </div>
+     </main>
+   </div>
+</div>
